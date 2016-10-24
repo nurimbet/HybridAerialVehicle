ca9f866c305c54594ac0fa5676368b0449c9993e
nurimbet <nurimbet@gmail.com>
2016-10-24 05:37:50 -0700

    Add files via upload

---
+++ sdfReader.cpp
@@ -0,0 +1,144 @@
+
+/*
+ * Copyright (c) 2015, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Author(s): Karen Liu <karenliu@cc.gatech.edu>
+ *
+ * Georgia Tech Graphics Lab and Humanoid Robotics Lab
+ *
+ * Directed by Prof. C. Karen Liu and Prof. Mike Stilman
+ * <karenliu@cc.gatech.edu> <mstilman@cc.gatech.edu>
+ *
+ * This file is provided under the following "BSD-style" License:
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+const double default_speed_increment = 0.5;
+
+const int default_ik_iterations = 4500;
+
+const double default_force =  50.0; // N
+const int default_countdown = 100;  // Number of timesteps for applying force
+
+#include "dart/dart.h"
+#include "dart/dynamics/Skeleton.h"
+#include "dart/simulation/World.h"
+#include "dart/utils/sdf/SdfParser.h"
+
+using namespace dart::common;
+using namespace dart::dynamics;
+using namespace dart::simulation;
+using namespace dart::gui;
+using namespace dart::utils;
+using namespace dart::math;
+
+
+class MyWindow : public SimWindow
+{
+public:
+  /// Constructor
+  MyWindow(const WorldPtr& world)
+  : mForceCountDown(0),
+    mPositiveSign(true)
+  {
+    setWorld(world);
+    
+    //mController = std::unique_ptr<Controller>
+    //    (new Controller(mWorld->getSkeleton("chicago")));
+  }
+  
+  /// Handle keyboard input
+
+protected:
+  
+  /// Number of iterations before clearing a force entry
+  int mForceCountDown;
+  
+  /// Whether a force should be applied in the positive or negative direction
+  bool mPositiveSign;
+  
+};
+
+// Load a chicago model and enable joint limits and self-collision
+SkeletonPtr loadchicago()
+{
+  
+  //TODO: Olzhas Don't Judge the absolute path!
+  SkeletonPtr chicago = SdfParser::readSkeleton("/home/nurimbet/Downloads/Chicago.sdf");
+
+  return chicago;
+}
+
+
+SkeletonPtr createFloor()
+{
+  SkeletonPtr floor = Skeleton::create("floor");
+  
+  // Give the floor a body
+  BodyNodePtr body =
+      floor->createJointAndBodyNodePair<WeldJoint>(nullptr).second;
+  
+  // Give the body a shape
+  double floor_width = 10000.0;
+  double floor_height = 0.01;
+  std::shared_ptr<BoxShape> box(
+      new BoxShape(Eigen::Vector3d(floor_width, floor_height, floor_width)));
+  box->setColor(dart::Color::Black());
+  
+  body->addVisualizationShape(box);
+  body->addCollisionShape(box);
+  
+  // Put the body into position
+  Eigen::Isometry3d tf(Eigen::Isometry3d::Identity());
+  tf.translation() = Eigen::Vector3d(0.0, -1.0, 0.0);
+  body->getParentJoint()->setTransformFromParentBodyNode(tf);
+  
+  return floor;
+}
+
+int main(int argc, char* argv[])
+{
+  SkeletonPtr floor = createFloor();
+
+  // Lesson 1
+  SkeletonPtr chicago = loadchicago();
+  
+  
+  WorldPtr world = std::make_shared<World>();
+  world->setGravity(Eigen::Vector3d(0.0, -9.81, 0.0));
+
+  
+  world->addSkeleton(floor);
+  world->addSkeleton(chicago);
+  
+  // Create a window for rendering the world and handling user input
+  MyWindow window(world);
+
+  // Initialize glut, initialize the window, and begin the glut event loop
+  glutInit(&argc, argv);
+  window.initWindow(640*2, 480*2, "Chicago Skyline");
+  glutMainLoop();
+}
